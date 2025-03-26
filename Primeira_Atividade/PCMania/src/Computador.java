37afc867ea3065a669966c9d18870a445b517876
Felipe Tagawa <137126826+OKamizura@users.noreply.github.com>
2025-03-26 15:08:51 -0300

    Add files via upload

--- Primeira_Atividade/PCMania/src/Computador.java
+++ Primeira_Atividade/PCMania/src/Computador.java
@@ -5,45 +5,66 @@ public class Computador {
 
     HardwareBasico[]hw;
     OS sistema;
-    MemoriaUSB mem;
+    MemoriaUSB mem; // Agregação.
 
 
 
     public Computador(String marca, float preco) {
         this.marca = marca;
         this.preco = preco;
-        hw = new HardwareBasico[3]; // Composição
-        sistema = new OS(); // Composição
+        hw = new HardwareBasico[3]; // Composição.
+        sistema = new OS(); // Composição.
     }
 
 
-    public void mostraPCConfigs(){
-        System.out.println("Marca: " + this.marca);
-        System.out.println("Preco: " + this.preco);
-        for(int i = 0; i < hw.length; i++){
-            if(hw[i] != null) {
-                System.out.println("Hardware: " + hw[i].nome + " de capacidade: " + hw[i].capacidade);
+    // O uso das %- :  apenas para deixar a saída mais elegante.
+    public void mostraPCConfigs() {
+        System.out.println("|-------------------------------------------------------|");
+        System.out.printf("| %-53s | ", "Marca: " + this.marca);
+        System.out.println();
+        System.out.printf("| %-53s | ", "Preço: R$ " + this.preco);
+        System.out.println();
+
+        for (int i = 0; i < hw.length; i++) {
+            if (hw[i] != null) {
+                if(hw[i].nome.startsWith("Pentium")) { // startsWith nos deixa não ter que repetir os "or" para i3,i5 e i7.
+                    System.out.printf("| %-53s | ", "Hardware 1: " + hw[i].nome + " de capacidade " + hw[i].capacidade + " MHz");
+                    System.out.println();
+                }
+                else{
+                    System.out.printf("| %-53s | ", "Hardware " + (i + 1) + ": " + hw[i].nome + " de capacidade " + hw[i].capacidade + " Gb");
+                    System.out.println();
+                }
+            } else {
+                System.out.printf("| %-53s | ", "Não há hardware --- PROBLEMA!");
+                System.out.println();
             }
-            else{
-                System.out.println("Não há hardware --- PROBLEMA!");
+        }
+
+        System.out.printf("| %-53s | ", "Nome do Sistema Operacional: " + this.sistema.nome + "(" + this.sistema.tipo + " bits)");
+        System.out.println();
+
+        if (mem != null) {
+            if(this.mem.capacidade != 1000) {
+                System.out.printf("| %-53s | ", "Nome da memória: " + this.mem.nome + " de capacidade " + this.mem.capacidade + " Gb");
+                System.out.println();
             }
+            else {
+                System.out.printf("| %-53s | ", "Nome da memória: " + this.mem.nome + " de capacidade 1 Tb");
+                System.out.println();
+            }
+        } else {
+            System.out.printf("| %-52s | ", "Memória não existente!");
+            System.out.println();
         }
-        System.out.println("Nome do Sistema Operacional: " + this.sistema.nome + " de tipo " + this.sistema.tipo);
-        if(mem != null){
-        System.out.println("Nome da memória: " + this.mem.nome + " de capacidade " + this.mem.capacidade);
-        }
-        else {
-            System.out.println("Memória não existente!");
-        }
+
+        System.out.println("|-------------------------------------------------------|");
     }
 
     public void addMemoriaUSB(MemoriaUSB musb){
         if(mem == null){
             mem = musb;
-            System.out.println("Memória USB adicionada com sucesso!");
-        }
-        else{
-            System.out.println("ERRO: Já existe uma memória USB associada a este computador!");
+            System.out.println("- Memória USB adicionada com sucesso!");
         }
     }
 
--- Primeira_Atividade/PCMania/src/Main.java
+++ Primeira_Atividade/PCMania/src/Main.java
@@ -11,12 +11,11 @@ public class Main {
         Cliente cliente = new Cliente(nome, cpf);
 
         System.out.println("Veja nossas opções de computadores: ");
-        System.out.println();
 
         MemoriaUSB mem = new MemoriaUSB();
-        cliente.computador[0] = new Computador("Apple", 2037); // Matrícula.
-        cliente.computador[1] = new Computador("Samsung", 3721); // Matrícula + 1234.
-        cliente.computador[2] = new Computador("Dell", 7715); // Matrícula + 5678.
+        cliente.computador[0] = new Computador("Apple", 2037.f); // Matrícula.
+        cliente.computador[1] = new Computador("Samsung", 3721.f); // Matrícula + 1234.
+        cliente.computador[2] = new Computador("Dell", 7715.f); // Matrícula + 5678.
 
         for(int i = 0; i < 3; i++){ // Depois ajustar pra length.
             cliente.computador[i].hw[0] = new HardwareBasico();
@@ -26,6 +25,7 @@ public class Main {
             cliente.computador[i].mem = mem;
         }
 
+        System.out.println();
         System.out.println("Opção 1: ");
         System.out.println();
 
@@ -39,9 +39,10 @@ public class Main {
         cliente.computador[0].sistema.setTipo(32); // 32 bits
         cliente.computador[0].mem.setNome("Pen-Drive");
         cliente.computador[0].mem.setCapacidade(16); // 16Gb
-        cliente.computador[0].addMemoriaUSB(mem);
         cliente.computador[0].mostraPCConfigs(); // Mostrar as configurações do PC1.
+        cliente.computador[0].addMemoriaUSB(mem);
 
+        System.out.println();
         System.out.println("Opção 2: ");
         System.out.println();
 
@@ -55,11 +56,11 @@ public class Main {
         cliente.computador[1].sistema.setTipo(64); // 64 bits
         cliente.computador[1].mem.setNome("Pen-drive");
         cliente.computador[1].mem.setCapacidade(32); // 32Gb
-        cliente.computador[1].addMemoriaUSB(mem);
         cliente.computador[1].mostraPCConfigs(); // Mostrar as configurações do PC1.
+        cliente.computador[1].addMemoriaUSB(mem);
 
 
-
+        System.out.println();
         System.out.println("Opção 3: ");
         System.out.println();
 
@@ -73,46 +74,44 @@ public class Main {
         cliente.computador[2].sistema.setTipo(64); // 64 bits
         cliente.computador[2].mem.setNome("HD Externo");
         cliente.computador[2].mem.setCapacidade(1000); // 1000 Gb ou 1Tb
+        cliente.computador[2].mostraPCConfigs();
         cliente.computador[2].addMemoriaUSB(mem);
-        cliente.computador[2].mostraPCConfigs(); // Mostrar as configurações do PC1.
 
         int op; // Variável para opção de escolha do cliente.
-        boolean escolhido1 = false, escolhido2 = false, escolhido3 = false; // Aplicar a lógica de estoque.
+        boolean[] escolhido = new boolean[3]; // Aplicar a lógica de estoque, começam com false.
         float valorTotal = 0; // Valor total da compra do cliente.
         while(true) {
 
+            System.out.println();
             System.out.println("Escolha a sua opção(1 - 3) ou 0 para sair: ");
             op = entrada.nextInt();
-            if(op == 0 && !escolhido1 && !escolhido2 && !escolhido3){
-                System.out.println("Você não comprou nada!");
-                break;
-            }
+
             if(op == 0){
                 break;
             }
             switch (op) {
                 case 1:
-                    if (!escolhido1) {
+                    if (!escolhido[0]) {
                         System.out.println("Computador 1 comprado!");
-                        escolhido1 = true;
+                        escolhido[0] = true;
                         valorTotal += cliente.computador[0].preco;
                     } else {
                         System.out.println("Você já comprou o Computador 1!");
                     }
                     break;
                 case 2:
-                    if (!escolhido2) {
+                    if (!escolhido[1]) {
                         System.out.println("Computador 2 comprado!");
-                        escolhido2 = true;
+                        escolhido[1] = true;
                         valorTotal += cliente.computador[1].preco;
                     } else {
                         System.out.println("Você já comprou o Computador 2!");
                     }
                     break;
                 case 3:
-                    if (!escolhido3) {
+                    if (!escolhido[2]) {
                         System.out.println("Computador 3 comprado!");
-                        escolhido3 = true;
+                        escolhido[2] = true;
                         valorTotal += cliente.computador[2].preco;
                     } else {
                         System.out.println("Você já comprou o Computador 3!");
@@ -124,20 +123,33 @@ public class Main {
             }
         }
 
-        if(escolhido1 || escolhido2 || escolhido3) {
-            System.out.println("O(s) PC(s) comprado(s) é(são) : ");
-            if(escolhido1){
-                cliente.computador[0].mostraPCConfigs();
-            }
-            if(escolhido2){
-                cliente.computador[1].mostraPCConfigs();
-            }
-            if(escolhido3){
-                cliente.computador[2].mostraPCConfigs();
-            }
+        if (escolhido[0] || escolhido[1] || escolhido[2]) {
+            System.out.println("O(s) PC(s) comprado(s) é(são):");
             System.out.println();
-            System.out.print("O valor total das compras: " + cliente.calculaTotalCompra(valorTotal));
+
+            for(int i = 0; i < escolhido.length; i++){
+                if(escolhido[i]){
+                    cliente.computador[i].mostraPCConfigs();
+                }
+
+            }
+
+            // O uso das %- :  apenas para deixar a saída mais elegante.
+            if(escolhido[0] && escolhido[1] && escolhido[2]){
+                System.out.printf("| %-53s | ", "Você comprou todos os computadores disponíveis na loja!");
+                System.out.println();
+            }
+
+            System.out.printf("| %-53s | ", "O valor total das compras: R$ " + cliente.calculaTotalCompra(valorTotal));
+            System.out.println();
+            System.out.println("|-------------------------------------------------------|");
         }
+        else{
+            System.out.println("Nenhuma compra foi feita, esperamos que tenha mais interesse na próxima!");
+        }
+
+        System.out.println();
+        System.out.println("Obrigado " + cliente.nome + ", volte sempre!");
 
         entrada.close();
     }
