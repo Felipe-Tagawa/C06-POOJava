d2fbd79de063bafc6540f6e326b5bd78942863be
Felipe Tagawa <137126826+Felipe-Tagawa@users.noreply.github.com>
2025-04-01 10:33:23 -0300

    Add files via upload

--- Primeira_Atividade/PCMania/src/Cliente.java
+++ Primeira_Atividade/PCMania/src/Cliente.java
@@ -2,6 +2,8 @@ public class Cliente {
 
     public String nome;
     public long cpf;
+    public float valor;
+    public int []op = new int[3];
 
     Computador[]computador = new Computador[3];
 
@@ -10,9 +12,15 @@ public class Cliente {
         this.cpf = cpf;
     }
 
-    public float calculaTotalCompra(float valor){
+    public float calculaTotalCompra(){
+        for (int i = 0; i < op.length; i++) {
+            if (op[i] > 0 && i < computador.length && computador[i] != null) {
+                valor += op[i] * computador[i].preco;
+            }
+        }
 
         return valor;
+
     }
 
 
--- Primeira_Atividade/PCMania/src/Main.java
+++ Primeira_Atividade/PCMania/src/Main.java
@@ -5,7 +5,13 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
         System.out.print("Bem vindo à loja de computadores, primeiro insira o seu nome:  ");
+
         String nome = entrada.nextLine();
+        while(nome.isEmpty()) {
+            System.out.println("Insira um nome permitido: ");
+            nome = entrada.nextLine();
+        }
+
         System.out.print("Agora insira seu CPF para login: ");
         long cpf = Long.parseLong(entrada.nextLine());
         Cliente cliente = new Cliente(nome, cpf);
@@ -17,17 +23,13 @@ public class Main {
         cliente.computador[1] = new Computador("Samsung", 3721.f); // Matrícula + 1234.
         cliente.computador[2] = new Computador("Dell", 7715.f); // Matrícula + 5678.
 
-        for(int i = 0; i < 3; i++){ // Depois ajustar pra length.
+        for(int i = 0; i < cliente.computador.length; i++){
             cliente.computador[i].hw[0] = new HardwareBasico();
             cliente.computador[i].hw[1] = new HardwareBasico();
             cliente.computador[i].hw[2] = new HardwareBasico();
             cliente.computador[i].sistema = new OS();
             cliente.computador[i].mem = mem;
         }
-
-        System.out.println();
-        System.out.println("Opção 1: ");
-        System.out.println();
 
         cliente.computador[0].hw[0].setNome("Pentium Core i3");
         cliente.computador[0].hw[1].setNome("Memoria RAM");
@@ -37,14 +39,10 @@ public class Main {
         cliente.computador[0].hw[2].setCapacidade(500); // 500 Gb HD.
         cliente.computador[0].sistema.setNome("Linux Ubuntu");
         cliente.computador[0].sistema.setTipo(32); // 32 bits
+        cliente.computador[0].addMemoriaUSB(mem);
         cliente.computador[0].mem.setNome("Pen-Drive");
         cliente.computador[0].mem.setCapacidade(16); // 16Gb
-        cliente.computador[0].mostraPCConfigs(); // Mostrar as configurações do PC1.
         cliente.computador[0].addMemoriaUSB(mem);
-
-        System.out.println();
-        System.out.println("Opção 2: ");
-        System.out.println();
 
         cliente.computador[1].hw[0].setNome("Pentium Core i5");
         cliente.computador[1].hw[1].setNome("Memoria RAM");
@@ -54,15 +52,10 @@ public class Main {
         cliente.computador[1].hw[2].setCapacidade(1000); // 1000Gb HD ou 1Tb.
         cliente.computador[1].sistema.setNome("Windows 8");
         cliente.computador[1].sistema.setTipo(64); // 64 bits
+        cliente.computador[0].addMemoriaUSB(mem);
         cliente.computador[1].mem.setNome("Pen-drive");
         cliente.computador[1].mem.setCapacidade(32); // 32Gb
-        cliente.computador[1].mostraPCConfigs(); // Mostrar as configurações do PC1.
         cliente.computador[1].addMemoriaUSB(mem);
-
-
-        System.out.println();
-        System.out.println("Opção 3: ");
-        System.out.println();
 
         cliente.computador[2].hw[0].setNome("Pentium Core i7");
         cliente.computador[2].hw[1].setNome("Memoria RAM");
@@ -72,18 +65,69 @@ public class Main {
         cliente.computador[2].hw[2].setCapacidade(2000); // 2000 Gb HD ou 2Tb.
         cliente.computador[2].sistema.setNome("Windows 10");
         cliente.computador[2].sistema.setTipo(64); // 64 bits
+        cliente.computador[0].addMemoriaUSB(mem);
         cliente.computador[2].mem.setNome("HD Externo");
         cliente.computador[2].mem.setCapacidade(1000); // 1000 Gb ou 1Tb
-        cliente.computador[2].mostraPCConfigs();
         cliente.computador[2].addMemoriaUSB(mem);
+
+        System.out.println();
+        System.out.println("Escolha entre as opções de promoção a seguir para visualizar: ");
+        System.out.println();
+        System.out.println("Modelo 1 - Mais Simples e Barato.");
+        System.out.println("Modelo 2 - Intermediário.");
+        System.out.println("Modelo 3 - Mais Robusto e Caro.");
+        System.out.println("4 - Ir para as compras.");
+        System.out.println();
+
+        int opcao;
+        do {
+            opcao = entrada.nextInt();
+            switch (opcao) {
+                case 1:
+                    cliente.computador[0].mostraPCConfigs();
+                    System.out.println();
+                    System.out.println("Modelo 1 - Mais Simples e Barato.");
+                    System.out.println("Modelo 2 - Intermediário.");
+                    System.out.println("Modelo 3 - Mais Robusto e Caro.");
+                    System.out.println("4 - Ir para as compras.");
+                    System.out.println();
+                    break;
+                case 2:
+                    cliente.computador[1].mostraPCConfigs();
+                    System.out.println();
+                    System.out.println("Modelo 1 - Mais Simples e Barato.");
+                    System.out.println("Modelo 2 - Intermediário.");
+                    System.out.println("Modelo 3 - Mais Robusto e Caro.");
+                    System.out.println("4 - Ir para as compras.");
+                    System.out.println();
+                    break;
+                case 3:
+                    cliente.computador[2].mostraPCConfigs();
+                    System.out.println();
+                    System.out.println("Modelo 1 - Mais Simples e Barato.");
+                    System.out.println("Modelo 2 - Intermediário.");
+                    System.out.println("Modelo 3 - Mais Robusto e Caro.");
+                    System.out.println("4 - Ir para as compras.");
+                    System.out.println();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Escreva um valor válido.");
+            }
+        }while(opcao != 4);
 
         int op; // Variável para opção de escolha do cliente.
         boolean[] escolhido = new boolean[3]; // Aplicar a lógica de estoque, começam com false.
-        float valorTotal = 0; // Valor total da compra do cliente.
+
+        // Fiz uma lógica de no máximo 3 computadores de cada promoção poderem ser comprados por cada usuário(estoque).
+        boolean[] compradoFull = new boolean[3]; // Vai ser verdadeira se todos os computadores de tal promoção se esgotarem.
+
+        System.out.println("Agora que já está ciente das promoções");
         while(true) {
 
             System.out.println();
-            System.out.println("Escolha a sua opção(1 - 3) ou 0 para sair: ");
+            System.out.println("Escolha sua opção(1 - 3) ou 0 para sair:");
             op = entrada.nextInt();
 
             if(op == 0){
@@ -91,30 +135,40 @@ public class Main {
             }
             switch (op) {
                 case 1:
-                    if (!escolhido[0]) {
+                    if(cliente.op[0] < 3) {
                         System.out.println("Computador 1 comprado!");
                         escolhido[0] = true;
-                        valorTotal += cliente.computador[0].preco;
-                    } else {
-                        System.out.println("Você já comprou o Computador 1!");
+                        cliente.op[0]++;
+                        if(cliente.op[0] == 3) {
+                            compradoFull[0] = true;
+                        }
+                    }
+                    else {
+                        System.out.println("Você já comprou o Computador 1 o número máximo de vezes!");
                     }
                     break;
                 case 2:
-                    if (!escolhido[1]) {
+                    if (cliente.op[1] < 3) {
                         System.out.println("Computador 2 comprado!");
                         escolhido[1] = true;
-                        valorTotal += cliente.computador[1].preco;
+                        cliente.op[1]++;
+                        if(cliente.op[1] == 3) {
+                            compradoFull[1] = true;
+                        }
                     } else {
-                        System.out.println("Você já comprou o Computador 2!");
+                        System.out.println("Você já comprou o Computador 2, o número máximo de vezes!");
                     }
                     break;
                 case 3:
-                    if (!escolhido[2]) {
+                    if (cliente.op[2] < 3) {
                         System.out.println("Computador 3 comprado!");
                         escolhido[2] = true;
-                        valorTotal += cliente.computador[2].preco;
+                        cliente.op[2]++;
+                        if(cliente.op[2] == 3) {
+                            compradoFull[2] = true;
+                        }
                     } else {
-                        System.out.println("Você já comprou o Computador 3!");
+                        System.out.println("Você já comprou o Computador 3, o número máximo de vezes!");
                     }
                     break;
                 default:
@@ -127,20 +181,26 @@ public class Main {
             System.out.println("O(s) PC(s) comprado(s) é(são):");
             System.out.println();
 
-            for(int i = 0; i < escolhido.length; i++){
+            for(int i = 0; i < cliente.computador.length; i++){
                 if(escolhido[i]){
                     cliente.computador[i].mostraPCConfigs();
                 }
-
             }
 
+            System.out.printf("| %-53s | ","Você comprou " + cliente.op[0] + " " + cliente.computador[0].marca);
+            System.out.println();
+            System.out.printf("| %-53s | ","Você comprou " + cliente.op[1] + " " + cliente.computador[1].marca);
+            System.out.println();
+            System.out.printf("| %-53s | ","Você comprou " + cliente.op[2] + " " + cliente.computador[2].marca);
+            System.out.println();
+
             // O uso das %- :  apenas para deixar a saída mais elegante.
-            if(escolhido[0] && escolhido[1] && escolhido[2]){
-                System.out.printf("| %-53s | ", "Você comprou todos os computadores disponíveis na loja!");
+            if (compradoFull[0] && compradoFull[1] && compradoFull[2]) {
+                System.out.printf("| %-53s | ", "Você comprou todos os computadores disponíveis!");
                 System.out.println();
             }
 
-            System.out.printf("| %-53s | ", "O valor total das compras: R$ " + cliente.calculaTotalCompra(valorTotal));
+            System.out.printf("| %-53s | ", "O valor total das compras: R$ " + cliente.calculaTotalCompra());
             System.out.println();
             System.out.println("|-------------------------------------------------------|");
         }
